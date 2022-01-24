9934efea39a3171aac3594f4ff4eac906db92196
Dan Fleser <danfleser.dev@gmail.com>
2022-01-25 00:44:00 +0200

    feate: config"

--- components/common/SocialLinks.js
+++ components/common/SocialLinks.js
@@ -8,8 +8,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://hashnode.com/@danfleser">
             <Image
-              src={`${prefix}/images/hashnode.svg`}
-              src={require(prefix + "../../public/images/" + frontmatter.cover_image)}
+              src={require(prefix + "../../public/images/hashnode.svg")}
               alt="Hashnode logo"
               height={24}
               width={24}
@@ -19,7 +18,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://danfleser.medium.com/">
             <Image
-              src={`${prefix}/images/medium.svg`}
+              src={require(prefix + "../../public/images/medium.svg")}
               alt="Medium logo"
               height={24}
               width={24}
@@ -29,7 +28,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://dev.to/danfleser">
             <Image
-              src={`${prefix}/images/devto.svg`}
+              src={require(prefix + "../../public/images/devto.svg")}
               alt="Dev.to logo"
               height={24}
               width={24}
@@ -39,7 +38,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://hackernoon.com/u/danfleser">
             <Image
-              src={`${prefix}/images/hackernoon.jpeg`}
+              src={require(prefix + "../../public/images/hackernoon.jpeg")}
               alt="Hackernoon logo"
               height={24}
               width={24}
@@ -49,7 +48,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://twitter.com/danfleser">
             <Image
-              src={`${prefix}/images/twitter.svg`}
+              src={require(prefix + "../../public/images/twitter.svg")}
               alt="Twitter logo"
               height={24}
               width={24}
@@ -59,7 +58,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://www.facebook.com/fleser.dan">
             <Image
-              src={`${prefix}/images/facebook.svg`}
+              src={require(prefix + "../../public/images/facebook.svg")}
               alt="Facebook logo"
               height={24}
               width={24}
@@ -69,7 +68,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://www.linkedin.com/in/danfleser/">
             <Image
-              src={`${prefix}/images/linkedin-icon.svg`}
+              src={require(prefix + "../../public/images/linkedin-icon.svg")}
               alt="Linkedin logo"
               height={24}
               width={24}
@@ -79,7 +78,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://www.instagram.com/danfleser/">
             <Image
-              src={`${prefix}/images/instagram-icon.svg`}
+              src={require(prefix + "../../public/images/instagram-icon.svg")}
               alt="Instagram logo"
               height={24}
               width={24}
@@ -89,7 +88,7 @@ export default function SocialLinks() {
         <li>
           <a href="https://github.com/danfleser">
             <Image
-              src={`${prefix}/images/github-icon.svg`}
+              src={require(prefix + "../../public/images/github-icon.svg")}
               className="dark:invert"
               alt="GitHub logo"
               height={24}
